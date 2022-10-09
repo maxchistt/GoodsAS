caad181d19dad3eea9b74f3061b00f0321e87169
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2022-10-10 01:19:39 +0500

    separate datacontroller in console

---
+++ GoodsAS/DataController.cs
@@ -0,0 +1,120 @@
+﻿using Common.Interfaces;
+using Common.Models;
+using System.Text.Json;
+
+namespace GoodsAS_Console
+{
+    internal class DataController
+    {
+        private IDataStorage? dataStorage;
+
+        public DataController() { }
+
+        public DataController(IDataStorage? dataStorage)
+        {
+            setDataStorage(dataStorage);
+        }
+
+        public void setDataStorage(IDataStorage? dataStorage)
+        {
+            this.dataStorage = dataStorage;
+            defaultFillTable();
+            readTable();
+        }
+
+        private void defaultFillTable()
+        {
+            if (dataStorage == null) return;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Item item = new()
+                {
+                    Id = i,
+                    Name = "Product" + i,
+                    Description = "Description" + i,
+                    Category = "ProductType" + i,
+                    Cost = 100 + i
+                };
+                dataStorage.postItem(item);
+            }
+        }
+
+        public void deleteItem()
+        {
+            if (dataStorage == null) return;
+
+            Console.WriteLine("Deleting item");
+            Console.WriteLine("Enter Id to delete:");
+            var input = Console.ReadLine();
+            if (input == null) return;
+
+            int Num;
+            bool isNum = int.TryParse(input.Trim(), out Num);
+
+            if (isNum)
+            {
+                bool res = dataStorage.deleteItem(Num);
+                Console.WriteLine(res ? "Deleted" : "Error");
+            }
+            Console.WriteLine();
+        }
+
+        public void postItem()
+        {
+            if (dataStorage == null) return;
+
+            Console.WriteLine("Posting new item");
+
+            Item item = new();
+            bool itemBuildRes = true;
+            foreach (var prop in typeof(Item).GetProperties())
+            {
+                string name = prop.Name;
+                Type type = prop.PropertyType;
+
+                Console.WriteLine($"Enter property {name} with {type.Name} type:");
+                var input = Console.ReadLine();
+                if (input == null) return;
+                input = input.Trim().Normalize();
+
+                if (type == typeof(int) || type == typeof(double) || type == typeof(float))
+                {
+                    double Num;
+                    bool isNum = double.TryParse(input, out Num);
+
+                    if (isNum)
+                    {
+                        prop.SetValue(item, Convert.ChangeType(Num, prop.PropertyType));
+                    }
+                    else
+                    {
+                        itemBuildRes = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    prop.SetValue(item, input);
+                }
+            }
+            bool res = itemBuildRes ? dataStorage.postItem(item) : false;
+            Console.WriteLine(res ? "Posted" : "Error");
+            Console.WriteLine();
+        }
+
+        public void readTable()
+        {
+            if (dataStorage == null) return;
+
+            Console.WriteLine("Printing table:");
+            var res = dataStorage.getItems();
+            Console.WriteLine("---");
+            foreach (var item in res)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(item) + "\n---");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- GoodsAS/Program.cs
+++ GoodsAS/Program.cs
@@ -1,7 +1,4 @@
-﻿using Common.Interfaces;
-using Common.Models;
-using EmulatedStorage;
-using System.Text.Json;
+﻿using EmulatedStorage;
 
 namespace GoodsAS_Console
 {
@@ -12,16 +9,15 @@ namespace GoodsAS_Console
         private enum Todo
         { close, post, read, delete }
 
-        private static IDataStorage? dataStorage;
+        private static DataController? controller;
 
         private static void Main(string[] args)
         {
-            dataStorage = new EmulatedDataStorage();
+            controller = new DataController();
 
             Console.WriteLine(Title + "\n");
 
-            defaultFillTable();
-            readTable();
+            controller.setDataStorage(new EmulatedDataStorage());
 
             startInteractionProcess();
         }
@@ -39,21 +35,21 @@ namespace GoodsAS_Console
                 if (isNum)
                 {
                     bool exists = Enum.IsDefined(typeof(Todo), Num);
-                    if (exists)
+                    if (exists && controller != null)
                     {
                         Todo todo = (Todo)Num;
                         switch (todo)
                         {
                             case Todo.delete:
-                                deleteItem();
+                                controller.deleteItem();
                                 break;
 
                             case Todo.read:
-                                readTable();
+                                controller.readTable();
                                 break;
 
                             case Todo.post:
-                                postItem();
+                                controller.postItem();
                                 break;
                         }
                     }
@@ -82,101 +78,6 @@ namespace GoodsAS_Console
             text += $"To close          Esc\\Enter\n";
 
             Console.WriteLine(text);
-        }
-
-        private static void deleteItem()
-        {
-            if (dataStorage == null) return;
-
-            Console.WriteLine("Deleting item");
-            Console.WriteLine("Enter Id to delete:");
-            var input = Console.ReadLine();
-            if (input == null) return;
-
-            int Num;
-            bool isNum = int.TryParse(input.Trim(), out Num);
-
-            if (isNum)
-            {
-                bool res = dataStorage.deleteItem(Num);
-                Console.WriteLine(res ? "Deleted" : "Error");
-            }
-            Console.WriteLine();
-        }
-
-        private static void postItem()
-        {
-            if (dataStorage == null) return;
-
-            Console.WriteLine("Posting new item");
-
-            Item item = new();
-            bool itemBuildRes = true;
-            foreach (var prop in typeof(Item).GetProperties())
-            {
-                string name = prop.Name;
-                Type type = prop.PropertyType;
-
-                Console.WriteLine($"Enter property {name} with {type.Name} type:");
-                var input = Console.ReadLine();
-                if (input == null) return;
-                input = input.Trim().Normalize();
-
-                if (type == typeof(int) || type == typeof(double) || type == typeof(float))
-                {
-                    double Num;
-                    bool isNum = double.TryParse(input, out Num);
-
-                    if (isNum)
-                    {
-                        prop.SetValue(item, Convert.ChangeType(Num, prop.PropertyType));
-                    }
-                    else
-                    {
-                        itemBuildRes = false;
-                        break;
-                    }
-                }
-                else
-                {
-                    prop.SetValue(item, input);
-                }
-            }
-            bool res = itemBuildRes ? dataStorage.postItem(item) : false;
-            Console.WriteLine(res ? "Posted" : "Error");
-            Console.WriteLine();
-        }
-
-        private static void readTable()
-        {
-            if (dataStorage == null) return;
-
-            Console.WriteLine("Printing table:");
-            var res = dataStorage.getItems();
-            Console.WriteLine("---");
-            foreach (var item in res)
-            {
-                Console.WriteLine(JsonSerializer.Serialize(item) + "\n---");
-            }
-            Console.WriteLine();
-        }
-
-        private static void defaultFillTable()
-        {
-            if (dataStorage == null) return;
-
-            for (int i = 0; i < 5; i++)
-            {
-                Item item = new()
-                {
-                    Id = i,
-                    Name = "Product" + i,
-                    Description = "Description" + i,
-                    Category = "ProductType" + i,
-                    Cost = 100 + i
-                };
-                dataStorage.postItem(item);
-            }
         }
     }
 }
