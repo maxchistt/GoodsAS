04006a70f2b7f6a007dcc93c596268e2a69ca840
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2022-10-11 01:45:38 +0500

    make console app mvc

---
+++ GoodsAS/ConsoleView.cs
@@ -0,0 +1,139 @@
+﻿using Common.Models;
+using System.Text.Json;
+
+namespace GoodsAS_Console
+{
+    internal class ConsoleView
+    {
+        public event Action onPrint = () => { Console.WriteLine("Printing table:"); };
+
+        public event Action onPost = () => { Console.WriteLine("Posting new item"); };
+
+        public event Action onDelete = () => { Console.WriteLine("Deleting item"); };
+
+        private string Title = "GoodsAS console app";
+
+        public ConsoleView()
+        {
+            Console.WriteLine(Title + "\n");
+            //startInteractionProcess();
+        }
+
+        public int? getItemId()
+        {
+            Console.WriteLine("Enter Id of item:");
+            var input = Console.ReadLine();
+
+            if (input == null) return null;
+
+            int Num;
+            bool isNum = int.TryParse(input.Trim(), out Num);
+            return isNum ? Num : null;
+        }
+
+        public Item? getItem()
+        {
+            Console.WriteLine("Fill item fields");
+            Item? item = new();
+            foreach (var prop in typeof(Item).GetProperties())
+            {
+                string name = prop.Name;
+                Type type = prop.PropertyType;
+
+                Console.WriteLine($"Enter property {name} with {type.Name} type:");
+                var input = Console.ReadLine();
+                if (input == null) return null;
+                input = input.Trim().Normalize();
+
+                if (type == typeof(int) || type == typeof(double) || type == typeof(float))
+                {
+                    double Num;
+                    bool isNum = double.TryParse(input, out Num);
+
+                    if (isNum)
+                    {
+                        prop.SetValue(item, Convert.ChangeType(Num, prop.PropertyType));
+                    }
+                    else
+                    {
+                        item = null;
+                        break;
+                    }
+                }
+                else
+                {
+                    prop.SetValue(item, input);
+                }
+            }
+            return item;
+        }
+
+        public void displayResult(bool res)
+        {
+            Console.WriteLine(res ? "Success\n" : "Error\n");
+        }
+
+        public void printTable(List<Item> itemsList, string? tableName = null)
+        {
+            if (tableName != null) Console.WriteLine(tableName);
+            Console.WriteLine("---");
+            foreach (var item in itemsList)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(item) + "\n---");
+            }
+            Console.WriteLine();
+        }
+
+        private enum Todo
+        { close, post, print, delete }
+
+        public void startInteractionProcess()
+        {
+            writeInsruction();
+            while (true)
+            {
+                var input = Console.ReadKey(true);
+
+                int Num;
+
+                if (int.TryParse(input.KeyChar.ToString(), out Num) && Enum.IsDefined(typeof(Todo), Num))
+                {
+                    switch ((Todo)Num)
+                    {
+                        case Todo.print:
+                            onPrint();
+                            break;
+
+                        case Todo.post:
+                            onPost();
+                            break;
+
+                        case Todo.delete:
+                            onDelete();
+                            break;
+                    }
+                }
+                else if (input.Key == ConsoleKey.Enter || input.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Press Enter or Esc to close or any key to continue");
+                    input = Console.ReadKey(true);
+                    if (input.Key == ConsoleKey.Enter || input.Key == ConsoleKey.Escape) return;
+                    Console.WriteLine();
+                    writeInsruction();
+                }
+            }
+        }
+
+        private void writeInsruction()
+        {
+            string text = "Enter keys for next actions:\n";
+
+            text += $"To post item      {(int)Todo.post}\n";
+            text += $"To print table    {(int)Todo.print}\n";
+            text += $"To delete item    {(int)Todo.delete}\n";
+            text += $"To close          Esc\\Enter\n";
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ GoodsAS/Contoller.cs
@@ -0,0 +1,97 @@
+﻿using Common.Interfaces;
+using Common.Models;
+
+namespace GoodsAS_Console
+{
+    internal class Contoller
+    {
+        private IDataStorage? dataStorage;
+        private ConsoleView? view;
+
+        public Contoller()
+        { }
+
+        public Contoller(IDataStorage? dataStorage, ConsoleView view)
+        {
+            setDataStorage(dataStorage);
+            setView(view);
+        }
+
+        public void setView(ConsoleView view)
+        {
+            this.view = view;
+            this.view.onPrint += printItemsTable;
+            this.view.onPost += deleteItem;
+            this.view.onDelete += postItem;
+            //this.view.startInteractionProcess();
+        }
+
+        public void setDataStorage(IDataStorage? dataStorage)
+        {
+            this.dataStorage = dataStorage;
+            //defaultFillTable();
+            //printTable();
+        }
+
+        public bool start()
+        {
+            if (view == null || dataStorage == null) return false;
+            defaultFillTable();
+            printItemsTable();
+            view.startInteractionProcess();
+            return true;
+        }
+
+        private void defaultFillTable()
+        {
+            if (dataStorage == null) return;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Item item = new()
+                {
+                    Id = i,
+                    Name = "Product" + i,
+                    Description = "Description" + i,
+                    Category = "ProductType" + i,
+                    Cost = 100 + i
+                };
+                dataStorage.postItem(item);
+            }
+        }
+
+        public void deleteItem()
+        {
+            if (view == null || dataStorage == null) return;
+
+            int? id = view.getItemId();
+
+            bool res = false;
+            if (id != null)
+            {
+                res = dataStorage.deleteItem(id.Value);
+            }
+            view.displayResult(res);
+        }
+
+        public void postItem()
+        {
+            if (view == null || dataStorage == null) return;
+
+            var item = view.getItem();
+
+            bool res = item != null ? dataStorage.postItem(item) : false;
+
+            view.displayResult(res);
+        }
+
+        public void printItemsTable()
+        {
+            if (view == null || dataStorage == null) return;
+
+            var itemsList = dataStorage.getItems();
+
+            view.printTable(itemsList, "Items");
+        }
+    }
+}
